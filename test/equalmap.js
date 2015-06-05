9125192b76d3f14554f3edafa4c29e8b2661e79a
Bacra Woo <bacra.woo@gmail.com>
2015-06-05 18:57:07 +0800

    Update: equalmap && longest api

--- test/equalmap.js
+++ test/equalmap.js
@@ -1,20 +1,24 @@
-var equalmap = require('../lib/equalmap');
+var equalMap = require('../lib/equalmap');
 var Table = require('cli-table');
 
 function tableDiff(oldStrArr, newStrArr) {
-	var head = oldStrArr.map(function(v,k) {return k+':'+v});
+	var head = oldStrArr.map(function(v,k) {return (k+':'+v).split('').join('\n')});
 	var cols = newStrArr;
-	var mapData = equalmap(oldStrArr, newStrArr);
+	var mapData = equalMap.equalMap(oldStrArr, newStrArr);
 	head.unshift('');
 
 	var table = new Table({head: head});
-	table.push.apply(table, mapData.map(function(item, index) {
-		var item2 = item.slice();
+	table.push.apply(table, mapData.data.map(function(item, index) {
+		var item2 = item.map(function(val) {
+			return val ? 1 : 0;
+		});
 		item2.unshift(index+':'+cols[index]);
 		return item2;
 	}));
 	console.log('diff: %s, %s', oldStrArr.join(''), newStrArr.join(''));
-	console.log('longs: ', mapData.longs);
+	console.log('all longs: ', equalMap.longest(mapData));
+	console.log('longs(0<=x<=4, 0<=y<=6): ', equalMap.longest(mapData, {x:0,y:0}, {x:4, y:6}));
+	console.log('longs(1<=x<=4, 2<=y<=6): ', equalMap.longest(mapData, {x:1,y:2}, {x:4, y:6}));
 	console.log(table.toString());
 }
 
